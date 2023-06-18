6c50645eb6064cbba5129cb0fc1e345531cc440d
Jaloliddin-Zokirov <jaloliddinzokirov.dev@gmail.com>
2023-06-18 10:09:43 +0500

    Initial commit

---
+++ src/components/StoreWrapper/StoreWrapper.js
@@ -0,0 +1,34 @@
+import React, { createContext, useReducer } from "react";
+
+export const StoreContext = createContext();
+
+const StoreWrapper = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <StoreContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreWrapper;
+
+const initialState = {
+  loading: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+
+    case "loading": {
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
