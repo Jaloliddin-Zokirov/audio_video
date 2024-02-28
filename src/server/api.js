bead1ce0a0b52f46176ec492d729887d7383d9d9
Jaloliddin-Zokirov <jaloliddinzokirov.dev@gmail.com>
2024-02-28 22:46:16 +0500

    edit api.js file

--- src/server/api.js
+++ src/server/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://sea-lion-app-xul26.ondigitalocean.app/";
-const Forimage = "https://sea-lion-app-xul26.ondigitalocean.app/";
+axios.defaults.baseURL = "https://sea-lion-app-xul26.ondigitalocean.app";
+const Forimage = "https://sea-lion-app-xul26.ondigitalocean.app";
 
 export { axios, Forimage };
